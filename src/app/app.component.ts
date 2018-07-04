1f003074442ae925cf2aea5063cda3401be39023
Mario Serrano <maseflores79@gmail.com>
2018-07-04 14:09:30 -0700

    Activado seguimiento en mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,62 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'Choferes';
-  lat = 51.678418;
-  lng = 7.809007;
+  lat = 32.678418;
+  lng = -115.3882286;
+  init = false;
 
-  items: Observable<any[]>;
+  siguiendoA: any = null;
+  siguiendoNombre: string = null;
+
+  items: Chofer[] = [];
+
   constructor(db: AngularFirestore) {
-    this.items = db.collection('usuarios').valueChanges();
+      db.collection('usuarios')
+            .valueChanges()
+            .subscribe ( (data: Chofer[]) => {
+                this.items = data;
+                if ( !this.init ) {
+                  /*
+                  en firebase estan en string, pero aqui me lo pide number,
+                  con el operador + me deja parsearlo a numero.
+                  */
+                  this.lat = +data[0].lat;
+                  this.lng = +data[0].lon;
+                }
+
+                if ( this.siguiendoA ) {
+                  data.forEach( chofer => {
+                    if (chofer.clave === this.siguiendoA) {
+                        this.actualizarPosicion(chofer);
+                    }
+                  });
+                }
+            });
   }
 
+  actualizarPosicion(chofer: Chofer) {
+    this.lat = +chofer.lat;
+    this.lng = +chofer.lon;
+  }
+
+  seguir( chofer: Chofer ) {
+    this.siguiendoA = chofer.clave;
+    this.siguiendoNombre = chofer.nombre;
+    this.actualizarPosicion(chofer);
+  }
+
+  dejarDeSeguir() {
+    this.siguiendoA = null;
+    this.siguiendoNombre = null;
+  }
+
+}
+
+
+
+interface Chofer {
+  nombre: string;
+  clave: string;
+  lat: number;
+  lon: number;
 }
