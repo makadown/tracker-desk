164c1ab9b9492616594219801f69f91c28bd2dcf
Mario Serrano <maseflores79@gmail.com>
2018-07-04 10:55:21 -0700

    metiendo bootstrap y gmaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Choferes';
+  lat = 51.678418;
+  lng = 7.809007;
 }
